f9f39d012d82f19b79a2b852d997e5573522fdf5
USFJUM <102630173+USFJUM@users.noreply.github.com>
2023-05-04 01:40:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+cap = cv2.VideoCapture(0)
+scaling_factor = 1
+KMS=cv2.imread('image1.jpg')
+while True:
+
+
+        ret, frame = cap.read()
+
+
+        frame = cv2.resize(frame, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
+
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+       
+
+        face_rects = face_cascade.detectMultiScale(gray, 1.3, 3)
+        for (x,y,w,h) in face_rects:
+                print('OK>>>','x=',x,'y=',y,'(h*w)/10=',int((h*w)/5000))
+                KMS=cv2.imread('image1.jpg')
+                KMS=cv2.resize(KMS,(w,h+50))
+                frame[y:y+h+50,x:x+w,:]=KMS
+                #cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
+                
+                cv2.imshow('Face Detector', frame)
+                #cv2.imshow('Face Detector2', kimso)
+                c = cv2.waitKey(1)
+        #if c == 27:
+                # break
+cap.release()
+cv2.destroyAllWindows()
